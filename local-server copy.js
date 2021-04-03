4d30e2c06d3c6df011e91db5e2e30fd3e9c99459
dAFI <dafinadavid@gmail.com>
2021-04-03 23:28:39 +0300

    json-server setup done, R ead data from server done, C creat new data to server done too

---
+++ local-server copy.js
@@ -0,0 +1,82 @@
+const todoForm = document.querySelector("[data-todo-form]");
+const todoInput = todoForm.querySelector("input[name='title']");
+const addTodoBtn = todoForm.querySelector("button[type='button']");
+const deleteTodoBtn = todoForm.querySelector("[data-todo-delete]");
+const todoList = document.querySelector("[data-todo-list]");
+
+let localTodos = [];
+
+if (!localStorage.getItem("todos")) {
+  localStorage.setItem("todos", JSON.stringify(localTodos));
+}
+
+function handleAddTodo(todoValue, status, id = "") {
+  const todo = document.createElement("p");
+  const input = document.createElement("input");
+  input.type = "checkbox";
+  input.id = id;
+  input.value = todoValue;
+  input.addEventListener("change", handleCheckTodo);
+  input.checked = status;
+  const label = document.createElement("label");
+  label.setAttribute("for", todoValue);
+  label.innerText = todoValue;
+  todo.appendChild(input);
+  todo.appendChild(label);
+
+  todoList.appendChild(todo);
+
+  localTodos.push({ todo: todoValue, isChecked: status });
+
+  updateLocal();
+
+  todoInput.value = "";
+}
+
+function updateLocal() {
+  localStorage.setItem("todos", JSON.stringify(localTodos));
+}
+
+function fetchTodos() {
+  todoList.innerHTML = "";
+  const todos = JSON.parse(localStorage.getItem("todos"));
+
+  todos.forEach((item, id) => {
+    handleAddTodo(item.todo, item.isChecked, id);
+  });
+}
+
+function handleCheckTodo(e) {
+  localTodos.forEach((item) => {
+    if (item.todo === e.target.value) {
+      item.isChecked = e.target.checked;
+    }
+  });
+
+  updateLocal();
+}
+
+function handleDeleteTodos() {
+  localTodos = localTodos.filter((item) => item.isChecked === false);
+
+  updateLocal();
+
+  location = "/";
+}
+
+addTodoBtn.addEventListener("click", function () {
+  handleAddTodo(todoInput.value, false);
+});
+
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    handleAddTodo(todoInput.value, false);
+  }
+});
+
+deleteTodoBtn.addEventListener("click", handleDeleteTodos);
+
+document.addEventListener("DOMContentLoaded", fetchTodos);
+
+// localStorage.clear();
--- local-server.js
+++ local-server.js
@@ -4,79 +4,95 @@ const addTodoBtn = todoForm.querySelector("button[type='button']");
 const deleteTodoBtn = todoForm.querySelector("[data-todo-delete]");
 const todoList = document.querySelector("[data-todo-list]");
 
-let localTodos = [];
+const apiUrl = "http://localhost:3000/todos";
 
-if (!localStorage.getItem("todos")) {
-  localStorage.setItem("todos", JSON.stringify(localTodos));
+function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error("Bad response from server");
 }
 
-function handleAddTodo(todoValue, status) {
+function renderTodo(data) {
   const todo = document.createElement("p");
   const input = document.createElement("input");
   input.type = "checkbox";
-  input.id = "todo_1";
-  input.value = todoValue;
+  input.id = data.id;
+  input.value = data.todo;
   input.addEventListener("change", handleCheckTodo);
-  input.checked = status;
+  input.checked = data.status;
   const label = document.createElement("label");
-  label.setAttribute("for", todoValue);
-  label.innerText = todoValue;
+  label.setAttribute("for", data.todo);
+  label.innerText = data.todo;
   todo.appendChild(input);
   todo.appendChild(label);
+  todoInput.value = "";
 
   todoList.appendChild(todo);
-
-  localTodos.push({ todo: todoValue, isChecked: status });
-
-  updateLocal();
-
-  todoInput.value = "";
 }
 
-function updateLocal() {
-  localStorage.setItem("todos", JSON.stringify(localTodos));
-}
-
-function persistTodo() {
+async function init(newTodo) {
   todoList.innerHTML = "";
-  const todos = JSON.parse(localStorage.getItem("todos"));
 
-  todos.forEach((item) => {
-    handleAddTodo(item.todo, item.isChecked);
+  const todos = await fetch(apiUrl)
+    .then(handleResponse)
+    .then((data) => {
+      data.forEach((item) => {
+        const todoItem = renderTodo(item);
+      });
+    });
+}
+
+async function handleAddTodo(e) {
+  e.preventDefault();
+  if (todoInput.value === "") {
+    return;
+  }
+
+  const newTodo = {
+    todo: todoInput.value,
+    isChecked: false,
+    authorId: 1,
+  };
+
+  renderTodo(newTodo);
+
+  const todoToServer = await fetch(apiUrl, {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+    headers: {
+      "Content-type": "application/json",
+    },
   });
 }
 
 function handleCheckTodo(e) {
-  localTodos.forEach((item) => {
+  todos.forEach((item) => {
     if (item.todo === e.target.value) {
       item.isChecked = e.target.checked;
     }
   });
-
-  updateLocal();
 }
 
 function handleDeleteTodos() {
-  localTodos = localTodos.filter((item) => item.isChecked === false);
-
-  updateLocal();
+  todos = todos.filter((item) => item.isChecked === false);
 
   location = "/";
 }
 
-addTodoBtn.addEventListener("click", function () {
-  handleAddTodo(todoInput.value, false);
+addTodoBtn.addEventListener("click", function (e) {
+  handleAddTodo(e);
 });
 
 document.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     e.preventDefault();
-    handleAddTodo(todoInput.value, false);
+    handleAddTodo();
   }
 });
 
 deleteTodoBtn.addEventListener("click", handleDeleteTodos);
 
-document.addEventListener("DOMContentLoaded", persistTodo);
+document.addEventListener("DOMContentLoaded", init);
 
 // localStorage.clear();
